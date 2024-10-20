a70fd89a8393f9211facd17b0e8097a620fcf2c9
AdamRomero17 <jhonadamabecillaromero@gmail.com>
2024-10-20 14:42:16 +0800

    looping.

--- JavaProjects/src/main/java/com/practicecode/javaprojects/JavaProjects.java
+++ JavaProjects/src/main/java/com/practicecode/javaprojects/JavaProjects.java
@@ -11,12 +11,26 @@ package com.practicecode.javaprojects;
 public class JavaProjects {
 
     public static void main(String[] args) {
-       int star1 = 5;
-       for(int i = 0; i < star1; i++){
+        //RIGHT TRIANGLE
+       /*int star1 = 5;
+       for(int i = 1; i <= star1; i++){
            for(int j = 1; j <= i; j++){
-              System.out.println(j);
+              System.out.print("*");
            }
-       };
+           System.out.println("");
+       }*/
+       
+       //EQUILATERAL
+       int star = 5;
+       for(int i = 1; i <= star; i++){
+           for(int j = i; j < star; j++){
+               System.out.print(".");
+           }
+           for(int j = 1; j <= (2 * i - 1); j++){
+               System.out.print(j);
+           }
+          System.out.println(); 
+       }
        
     }
 }
